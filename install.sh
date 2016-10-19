bc09103ce849bba19ce9d47432a09f1f36c6e61d
rzavalet <zavaleta.vazquez.ricardo.jose@gmail.com>
2016-10-19 08:31:46 -0500

    Addded install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+files="bashrc bash_aliases vimrc"
+BAKDIR=$HOME/backup_config_files
+
+for i in $files
+do
+  TMP=.${i}
+  if [ -e $HOME/$TMP ]
+  then
+    if [ ! -d $BAKDIR ]
+    then
+      echo "Creating backup directory: $BAKDIR"
+      mkdir $BAKDIR
+    fi
+    echo "Moving $HOME/$TMP to $BAKDIR/$i.bak"
+    mv $HOME/$TMP $BAKDIR/$i.bak 
+  fi
+  echo "Installing files: $i"
+  cp $i $HOME/$TMP
+done
