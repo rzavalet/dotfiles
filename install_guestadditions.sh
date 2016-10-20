1aaeee83e0414bf050b9e1aeaacfff069fd1f966
rzavalet <zavaleta.vazquez.ricardo.jose@gmail.com>
2016-10-20 03:49:09 -0500

    Added new script

---
+++ install_guestadditions.sh
@@ -0,0 +1,6 @@
+#-----------------------------------------------------------
+# Run this script to install vbox's guest additions
+# on a linux host
+#-----------------------------------------------------------
+sudo apt-get install dkms build-essential linux-headers-generic
+
